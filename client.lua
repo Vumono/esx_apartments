c3fbbe0d62a851849e9883dd1b6e7e0761180ef7
Vumono <87247208+Vumono@users.noreply.github.com>
2022-01-16 12:55:11 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,4 @@
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
+local inapartment = false
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(PlayerData)
@@ -97,6 +87,24 @@ Citizen.CreateThread(function()
 	end
 end)
 
+Citizen.CreateThread(function()
+	local playerPool = {}
+	while inapartment do
+		local players = GetActivePlayers()
+		for i=1, #players do
+			local player = players[i]
+			if player ~= PlayerId() and not playerPool[player] then
+				playerPool[player] = true
+				NetworkConcealPlayer(player, true, true)
+			end
+		end
+		Citizen.Wait(500)
+	end
+	for k in pairs(playerPool) do
+		NetworkConcealPlayer(k, false, false)
+	end
+end)
+
 RegisterNetEvent('esx_apartments:changeclothingmenu', function()
 	TriggerEvent('nh-context:sendMenu', {
 		{
@@ -136,22 +144,31 @@ end)
 
 RegisterNetEvent('esx_apartments:exit')
 AddEventHandler('esx_apartments:exit', function()
-    StartScreenEffect("SuccessNeutral",  3000,  false)
+	local playerId, playerPed = PlayerId(), PlayerPedId()
+    inapartment = false
+	SetEntityVisible(playerPed, 1, 0)
+	SetPlayerInvincible(playerId, 0)
+	DoScreenFadeOut(1500)
 	SetEntityCoords(PlayerPedId(), -270.6047, -957.9693, 31.2231)
-    SetLocalPlayerVisibleLocally()
-    --SetEntityLocallyVisible(PlayerPedId())
-	SetEntityNoCollisionEntity(PlayerPedId(), ESX.Game.GetPlayersInArea(vector3(151.4854,-1007.5345,-99.0), 10.0), false)
+	DoScreenFadeIn(1500)
+
 end)
 
 RegisterNetEvent('esx_apartments:enterapartment')
 AddEventHandler('esx_apartments:enterapartment', function()
-    StartScreenEffect("SuccessNeutral",  3000,  false)
+    inapartment = true
+	DoScreenFadeOut(1500)
     SetEntityCoords(PlayerPedId(), 151.38, -1007.95, -99.0 - 1.0)
-    
-    --SetEntityLocallyInvisible(PlayerPedId())
-    SetLocalPlayerInvisibleLocally()
-	SetEntityNoCollisionEntity(PlayerPedId(), ESX.Game.GetPlayersInArea(vector3(151.4854,-1007.5345,-99.0), 10.0), true)
+	DoScreenFadeIn(1500)
+	while inapartment do
+		SetEntityVisible(PlayerPedId(), 0, 0)
+		SetLocalPlayerVisibleLocally(1)
+		SetPlayerInvincible(PlayerId(), 1)
+		Citizen.Wait(0)
+	end
 end)
+
+
 --[[
 RegisterNetEvent('esx_apartments:invite')  #nh-context and right export
 AddEventHandler('esx_apartments:invite', function()
